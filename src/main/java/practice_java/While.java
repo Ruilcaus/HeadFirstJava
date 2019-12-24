1a3efc7615189e4a189ce43817dda5de28c5927a
Ruilcaus <beder.alex@gmail.com>
2019-12-24 16:08:27 -0500

    Create While.java

---
+++ src/main/java/practice_java/While.java
@@ -0,0 +1,13 @@
+package practice_java;
+
+class While {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 10) {
+            a++;
+            System.out.println("a = " + a);
+        }
+            System.out.println("end of loop");
+
+    }
+}
