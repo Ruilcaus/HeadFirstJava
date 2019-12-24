cfb6249db90274d83bad118b6039fa2de942cacc
Ruilcaus <beder.alex@gmail.com>
2019-12-24 16:00:26 -0500

    p11 while loop

---
+++ src/main/java/practice_java/MyFirstApp.java
@@ -0,0 +1,13 @@
+package practice_java;
+
+class MyFirstApp {
+    public static void main(String[] args) {
+        int a = 3;
+        while (a <10) {
+            a=a+1;
+            System.out.println("a = " + a);
+        }
+            System.out.println("end of loop");
+
+    }
+}
