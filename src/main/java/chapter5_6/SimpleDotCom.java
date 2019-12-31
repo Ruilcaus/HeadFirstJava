d7783c328a5ac057d6211e19ccdbde21d9aeec9f
Ruilcaus <beder.alex@gmail.com>
2019-12-30 21:13:02 -0500

    chapter 5 simplified ver

--- src/main/java/chapter5_6/SimpleDotCom.java
+++ src/main/java/chapter5_6/SimpleDotCom.java
@@ -1,4 +1,28 @@
 package chapter5_6;
 
 public class SimpleDotCom {
+    private int[] locationCells;
+    private int numOfHits = 0;
+
+    void setLocationCells(int[] locs){
+        locationCells = locs;
+         }
+
+    public String checkYourself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result;
+    }
+
 }
